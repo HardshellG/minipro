5bd2d8b97aed9400deb836e65b2fe3968ccbc99a
Omarbil01 <112606217+Omarbil01@users.noreply.github.com>
2023-10-13 01:12:29 +0530

    Update jee.py

--- pages/jee.py
+++ pages/jee.py
@@ -8,7 +8,7 @@ df=pd.read_csv("Total Seats.csv")
 st.write(df)
 
 st.bar_chart(df, x="Branch of Study", y="Total Seats" )
-dg=pd.read_csv("C:/Users/Omar/Desktop/mysite/All IIT List of Percentile.csv")
+dg=pd.read_csv("All IIT List of Percentile.csv")
 st.write(dg)
 
 st.bar_chart(dg, x="IIT/Branch", y=["General","SC","ST","OBC"] )
